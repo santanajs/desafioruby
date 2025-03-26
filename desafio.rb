65dcef5654c447b085c07533dda5caaecf86fd18
santanajs <78940953+santanajs@users.noreply.github.com>
2025-03-25 23:17:01 -0300

    Add files via upload

---
+++ desafio.rb
@@ -0,0 +1,11 @@
+print "Digite seu nome: "
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+sobrenome = gets.chomp
+
+print  "Informe a sua idade? "
+idade = gets.chomp.to_i
+ 
+
+puts "Seu nome e sobrenome é #{nome} #{sobrenome} e sua idade é #{idade} anos"
